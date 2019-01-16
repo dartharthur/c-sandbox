9bf94a0368ab7d9ec54559470f0389702b3d0077
Kevin Arthur <kevin.arthur.roy@gmail.com>
2019-01-15 18:40:22 -0800

    add notes on exercises

---
+++ K&R-C/chapter-1/1.2.1-fToC.c
@@ -0,0 +1,24 @@
+/* Celsius to Fahrenheit: C = (5/9)(F-32) */
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ... 300 */
+
+int main()
+{
+    int fahr, celsius; /* in C, all variables must be declared befor they are used */
+    int lower, upper, step; /* statements end in semi-colons */
+
+    lower = 0;      /* lower limit of temperature scale */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9; /* mult by 5 first bc integer division discards fraction, 5/9 truncates to zero */
+        printf("%3d\t%6d\n", fahr, celsius); /* %d substituted positionally with integer arguments */
+        fahr = fahr + step; /* can augment %d with widths for prettier output */
+    }
+}
+
+/* problem: we have used integer arithmetic, so Celsius temperatures are inaccurate */
---
+++ K&R-C/chapter-1/1.2.2-fToC.c
@@ -0,0 +1,25 @@
+/* Celsius to Fahrenheit: C = (5/9)(F-32) */
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table 
+for fahr = 0, 20, ... 300 floating-point version */
+
+int main()
+{
+    float fahr, celsius; /* in C, all variables must be declared befor they are used */
+    float lower, upper, step; /* statements end in semi-colons */
+
+    lower = 0;      /* lower limit of temperature scale */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower; /* int is converted to float */
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0); /* decimal point in a constant indicates that it's a floating point */
+         /* %3.0: three char wide, no decimal point / fraction digits */
+         /* %6.1: six char wide, one digit after decimal point */
+        printf("%3.0f\t%6.1f\n", fahr, celsius); /* %f for floating point */
+        fahr = fahr + step; /* can augment %f with widths for prettier output */
+    }
+}
