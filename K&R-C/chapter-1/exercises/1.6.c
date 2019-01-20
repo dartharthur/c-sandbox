05690ed4ab0ede34fe30b9cc64c502046541ad75
Kevin Arthur <kevin.arthur.roy@gmail.com>
2019-01-19 22:45:33 -0800

    complete 1.5.1 exercises

---
+++ K&R-C/chapter-1/exercises/1.6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF) 
+    {
+        putchar(c);
+        printf("%d ", c != EOF);
+    }
+    printf("\n%d\n", c != EOF);
+    return 0;
+}
+
+/*
+gcc 1.6.c -o eof.out && ./eof.out < afile
+c1 o1 n1 t1 e1 n1 t1 s1
+0
+*/
